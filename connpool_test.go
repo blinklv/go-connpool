8447d1894bc82289314210f38c84df5be8de542e
blinklv <blinklv@icloud.com>
2018-07-12 11:46:25 +0800

    Add TestBucketPop.

--- connpool_test.go
+++ connpool_test.go
@@ -3,7 +3,7 @@
 // Author: blinklv <blinklv@icloud.com>
 // Create Time: 2018-07-11
 // Maintainer: blinklv <blinklv@icloud.com>
-// Last Change: 2018-07-11
+// Last Change: 2018-07-12
 
 package connpool
 
@@ -170,5 +170,82 @@ func TestBucketPush(t *testing.T) {
 		assert.Equal(t, sample.closedNumber, int(success+full+1))
 		assert.Equal(t, int(success), sample.b.size)
 		assert.Equal(t, sample.b.size, actualSize(sample.b))
+	}
+}
+
+func TestBucketPop(t *testing.T) {
+	samples := []struct {
+		b            *bucket
+		workers      int
+		opNumber     int
+		closedNumber int
+	}{
+		{&bucket{capacity: 128}, 1, 10000, 10001},
+		{&bucket{capacity: 256}, 4, 10000, 5001},
+		{&bucket{capacity: 1024}, 20, 10000, 5001},
+		{&bucket{capacity: 7000}, 32, 10000, 5001},
+	}
+
+	for _, sample := range samples {
+		var (
+			count   int64
+			success int64
+			fail    int64
+			end     = &sync.WaitGroup{}
+			pause   = &sync.WaitGroup{}
+			cont    = make(chan struct{})
+		)
+
+		pop := func() {
+			if sample.b.pop() != nil {
+				atomic.AddInt64(&success, 1)
+			} else {
+				atomic.AddInt64(&fail, 1)
+			}
+		}
+
+		for i := 0; i < sample.b.capacity; i++ {
+			sample.b.push(genConn(sample.b, "192.168.1.100:80"))
+		}
+
+		for worker := 0; worker < sample.workers; worker++ {
+			pause.Add(1)
+			end.Add(1)
+			go func() {
+				i := 0
+				for {
+					i = int(atomic.AddInt64(&count, 1))
+					if i >= sample.closedNumber {
+						break
+					}
+					pop()
+				}
+
+				pause.Done()
+				pause.Wait()
+				if i == sample.closedNumber {
+					sample.b.closed = true
+					close(cont)
+				}
+				<-cont
+
+				for i <= sample.opNumber {
+					pop()
+					i = int(atomic.AddInt64(&count, 1))
+				}
+
+				end.Done()
+			}()
+		}
+		end.Wait()
+
+		assert.Equal(t, sample.opNumber, int(success+fail))
+		if sample.b.capacity < sample.closedNumber {
+			assert.Equal(t, sample.b.capacity, int(success))
+			assert.Equal(t, 0, sample.b.size)
+		} else {
+			assert.Equal(t, sample.b.capacity-sample.closedNumber+1, sample.b.size)
+			assert.Equal(t, sample.closedNumber-1, int(success))
+		}
 	}
 }
