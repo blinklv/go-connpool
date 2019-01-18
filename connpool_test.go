09a36948ac5f7544d0883af8fbdf85d9e4949540
blinklv <blinklv@icloud.com>
2019-01-18 16:31:03 +0800

    test: add bucket.push

--- connpool_test.go
+++ connpool_test.go
@@ -8,43 +8,64 @@
 package connpool
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"net"
+	"sync"
 	"sync/atomic"
 	"testing"
+	"time"
 )
+
+func TestBucket(t *testing.T) {
+	t.Run("bucket.push", testBucketPush)
+}
 
 func testBucketPush(t *testing.T) {
 	for _, e := range []struct {
 		b *bucket
 		n int
 	}{
-		{b: &bucket{capacity: 0, top: &element{}}, n: 64},
-		{b: &bucket{capacity: 1, top: &element{}}, n: 64},
-		{b: &bucket{capacity: 32, top: &element{}}, n: 64},
-		{b: &bucket{capacity: 64, top: &element{}}, n: 64},
-		{b: &bucket{capacity: 128, top: &element{}}, n: 64},
+		{b: &bucket{capacity: 0, top: &element{}}, n: 4096},
+		{b: &bucket{capacity: 1, top: &element{}}, n: 4096},
+		{b: &bucket{capacity: 2048, top: &element{}}, n: 4096},
+		{b: &bucket{capacity: 4096, top: &element{}}, n: 4096},
+		{b: &bucket{capacity: 8192, top: &element{}}, n: 4096},
 	} {
-		d, succ, fail := &dialer{}, 0, 0
-		for i := 0; i < e.n; i++ {
+		var (
+			d          = &dialer{}
+			succ, fail int64
+		)
+
+		execute(16, e.n, func() {
 			conn, _ := d.dial("192.168.1.1:80")
-			if e.b.push(conn) {
-				succ++
+			c := e.b.bind(conn)
+			if e.b.push(c) {
+				atomic.AddInt64(&succ, 1)
 			} else {
-				fail++
+				atomic.AddInt64(&fail, 1)
+				c.Close()
 			}
-		}
+		})
+
+		assert, env := assert.New(t), sprintf("[capacity:%d number:%d]", e.b.capacity, e.n)
+		assert.Equalf(e.n, int(succ+fail), "%s total:%d != succ:%d + fail:%d", env, e.n, succ, fail)
+		assert.Equalf(min(e.b.capacity, e.n), int(succ), "%s min(capacity:%d, number:%d) != succ:%d", env, e.b.capacity, e.n, succ)
+		assert.Equalf(max(e.n-e.b.capacity, 0), int(fail), "%s max(number:%d - capacity:%d, 0) != fail:%d", env, e.n, e.b.capacity, fail)
+		assert.Equalf(e.b.size, int(succ), "%s bucket.size:%d != succ:%d", env, e.b.size, succ)
+		assert.Equalf(e.b.size, e.b._size(), "%s bucket.size:%d != bucket._size:%d", env, e.b.size, e.b._size())
+		assert.Equalf(e.b.total, succ, "%s bucket.total:%d != succ:%d", env, e.b.total, succ)
+		assert.Equalf(e.b.idle, succ, "%s bucket.idle:%d != succ:%d", env, e.b.idle, succ)
+		assert.Equalf(e.b.depth, 0, "%s bucket.depth:%d != 0", env, e.b.depth)
+		assert.Equalf(e.b.cut, (*element)(nil), "%s bucket.cut:%v != nil", env, e.b.cut)
 	}
 }
 
 // Executing a callback function n times in multiple goroutines simultaneously.
 func execute(parallel, n int, cb func()) {
-	for wg, pn := (&sync.WaitGroup{}), n/parallel; n > (1 - pn); n -= pn {
-		m := pn
-		if n < 0 {
-			m = pn + n
-		}
-
+	var wg = &sync.WaitGroup{}
+	for pn := n / parallel; n > 0; n -= pn {
+		m := min(pn, n)
 		wg.Add(1)
 		go func(m int) {
 			for i := 0; i < m; i++ {
@@ -58,10 +79,10 @@ func execute(parallel, n int, cb func()) {
 
 type dialer struct {
 	port  int32
-	count int32
+	count int64
 }
 
-func (d *dailer) dial(address string) (net.Conn, error) {
+func (d *dialer) dial(address string) (net.Conn, error) {
 	c := &connection{
 		d:      d,
 		local:  resolveTCPAddr(sprintf("127.0.0.1:%d", atomic.AddInt32(&d.port, 1))),
@@ -80,7 +101,7 @@ func resolveTCPAddr(s string) net.Addr {
 
 // connection is an auxiliary struct which satisfies the net.Conn interface.
 type connection struct {
-	d             *dailer
+	d             *dialer
 	local, remote net.Addr
 }
 
@@ -115,4 +136,18 @@ func (c *connection) SetReadDeadline(t time.Time) error {
 
 func (c *connection) SetWriteDeadline(t time.Time) error {
 	return nil
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
