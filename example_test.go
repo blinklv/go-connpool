ec0c81a29ca574bb6150709ddacf9a4b8ea24865
blinklv <blinklv@icloud.com>
2019-03-27 19:48:58 +0800

    add example

--- example_test.go
+++ example_test.go
@@ -23,6 +23,68 @@ import (
 	"time"
 )
 
+// The following example illustrates how to use this package in your client program.
+func Example() {
+	conn, err := pool.Get(selectAddress())
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	response, err := roundtrip(conn, "How long is forever?")
+	if err != nil {
+		conn.(*connpool.Conn).Release()
+		if conn, err = pool.New(selectAddress()); err != nil {
+			return
+		}
+		response, err = roundtrip(conn, "How long is forever?")
+	}
+
+	fmt.Printf("%s", response)
+	// Output:
+	// Sometimes, just one second.
+}
+
+func ExampleNew() {
+	dial := func(address string) (net.Conn, error) {
+		return net.Dial("tcp", address)
+	}
+
+	pool, err := connpool.New(dial, 128, 5*time.Minute)
+	if err != nil {
+		log.Fatalf("create connection pool failed %s", err)
+	}
+	pool.Close()
+}
+
+func ExamplePool_Get() {
+	conn, err := pool.Get(selectAddress())
+	if err != nil {
+		log.Fatalf("get a connection failed %s", err)
+	}
+	conn.Close()
+}
+
+func ExampleConn_Release() {
+	conn, _ := pool.New(selectAddress())
+	conn.(*connpool.Conn).Release()
+}
+
+/* Auxilairy Functions and Variables for Example */
+func dial(address string) (net.Conn, error) {
+	return &connection{}, nil
+}
+
+func selectAddress() string { return "" }
+
+func handle(net.Conn) error { return nil }
+
+func roundtrip(net.Conn, string) (string, error) {
+	return "Sometimes, just one second.", nil
+}
+
+var pool, _ = connpool.New(dial, 128, 5*time.Minute)
+
 /* Black Box Test */
 func TestPackage(t *testing.T) {
 	ss := servers{
